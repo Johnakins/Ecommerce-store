b3652d7cfbf9b86f24cd711b0dc56bba902a7f7c
Johnakins <johnolawole1@gmail.com>
2023-12-02 06:03:38 +0100

    Banner and Product Card components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import { products } from "@/utils/products";
+import Container from "./components/Container";
+import HeroBanner from "./components/HeroBanner";
+import { truncatetext } from "@/utils/truncatetext";
+import ProductCard from "./products/ProductCard";
 
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <div className="p-8">
+      <Container>
+        <div>
+          <HeroBanner/>
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {products.map((product:any)=>(
+            <ProductCard key={product.id} data={product} />
+          ))}
+        </div>
+      </Container>
+    </div>
   )
 }
