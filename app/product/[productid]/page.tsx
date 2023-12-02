14db91e647e990a0e1e47a451fa7c72d6c64eb9d
Johnakins <johnolawole1@gmail.com>
2023-12-02 15:06:55 +0100

    Single product page

---
+++ app/product/[productid]/page.tsx
@@ -0,0 +1,19 @@
+import Container from "@/app/components/Container"
+import ProductDetails from "./ProductDetails"
+import { product } from "@/utils/product"
+
+interface IPrams {
+    productId?: string
+}
+
+const Product = ({params}:{params: IPrams}) => {
+  return (
+    <div className="p-8">
+      <Container>
+        <ProductDetails product={product}/>
+      </Container>
+    </div>
+  )
+}
+
+export default Product
